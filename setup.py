ac257ed27f25bb1b55356eca8a72fdd87d16634b
schwarzan <44768487+schwarzan@users.noreply.github.com>
2022-09-01 11:15:35 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='custom_logreg',
+    version='0.0.1',
+    author='Anna Schwarz',
+    author_email='schwarz.annak@gmail.com',
+    packages=find_packages(),
+)
